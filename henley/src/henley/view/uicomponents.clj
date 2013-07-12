3d8018cc93f4a5ecab1041aa9fb60d04deb831b2
Marc Liberatore <liberato@cs.umass.edu>
2013-07-12 15:09:59 -0400

    no earmuffs for Vars that aren't dynamic

--- henley/src/henley/view/uicomponents.clj
+++ henley/src/henley/view/uicomponents.clj
@@ -1,10 +1,10 @@
 (ns henley.view.uicomponents
   (:import henley.uicomponents.MainFrame))
 
-(def *frame* (MainFrame.))
-(def *generate-button* (.generateButton *frame*))
-(def *select-button* (.selectButton *frame*))
-(def *input-file* (.inputFileText *frame*))
-(def *nb-words* (.numberOfWordsText *frame*))
-(def *generated-text* (.generatedText *frame*))
+(def frame (MainFrame.))
+(def generate-button (.generateButton frame))
+(def select-button (.selectButton frame))
+(def input-file (.inputFileText frame))
+(def nb-words (.numberOfWordsText frame))
+(def generated-text (.generatedText frame))
 
