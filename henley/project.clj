81a1dd7bcd24b7573577046ba92f6b3d4b3f316a
Marc Liberatore <liberato@cs.umass.edu>
2013-07-13 06:23:09 -0400

    typo fix

--- henley/project.clj
+++ henley/project.clj
@@ -1,6 +1,6 @@
 (defproject henley "1.0.0-SNAPSHOT"
   :description "A GUI for a text generator similar to the one
- in the book Ansi Common Lisp, Paul Graham"
+ in the book Ansi Common Lisp, by Paul Graham"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [henley/henleyuicomponents "1.0.0-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.2.0"]
