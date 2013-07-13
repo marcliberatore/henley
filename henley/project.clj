e4135bd79878406978cfb77a5767ac0a421dbf10
Marc Liberatore <liberato@cs.umass.edu>
2013-07-13 06:23:24 -0400

    remove old dev-dependencies

--- henley/project.clj
+++ henley/project.clj
@@ -3,7 +3,5 @@
  in the book Ansi Common Lisp, by Paul Graham"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [henley/henleyuicomponents "1.0.0-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.2.0"]
-                     [lein-run "1.0.0-SNAPSHOT"]]
 ;;  :main henley.core
   )
